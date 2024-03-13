3b37c03da17162f51a0d42df0a855c8d8dba4bd0
Deepak7009 <deepak7009verma@gmail.com>
2024-03-13 14:25:46 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import AllData from './components/AllData';
-import { Routes, Route } from 'react-router-dom';
-import DbData from './components/DbData';
+import AllData from "./components/AllData";
+import { Routes, Route } from "react-router-dom";
+import DbData from "./components/DbData";
+import Navbar from "./components/Navbar";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<AllData />} />
-                <Route path="/dbData" element={<DbData />} />
-            </Routes>
-            {/*<AllData />*/}
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<AllData />} />
+        <Route
+          path="/dbData"
+          element={
+            <>
+              <Navbar />
+              <DbData />
+            </>
+          }
+        />
+      </Routes>
+      {/*<AllData />*/}
+    </>
+  );
 }
 
 export default App;
